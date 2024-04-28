f1d8b3367fd29bee548cdc247dfc477a2ce0d8ed
Kshishtawy <kareememadelshishtawy@gmail.com>
2024-04-29 00:59:54 +0300

    Completing register passenger, and half part of login

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -1,60 +1,123 @@
-import { Link } from "react-router-dom";
-import './LoginPage.css'
-import image from './images/photo-1539787200876-3c033a7bebcd.jpeg';
+import { useState, useEffect, useReducer } from "react";
+import { Link, Redirect } from "react-router-dom";
+import "./LoginPage.css";
+import image from "./images/photo-1539787200876-3c033a7bebcd.jpeg";
 
 const LoginPage = () => {
-    return (
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
 
-        <div>
-            <section className=" secondColor" >
-                <div className="container my-5">
-                    <div className="row d-flex justify-content-center align-items-center ">
-                        <div className="col col-xl-10">
-                            <div className="card border rounded-3">
-                                <div className="row g-0">
-                                    <div className="col-md-6 col-lg-5 d-none d-md-block">
-                                        <img src={image}  alt="login form" className="img-fluid rounded-3" />
-                                    </div>
-                                    <div className="col-md-6 col-lg-7 d-flex align-items-center">
-                                        <div className="card-body  p-lg-5 text-black">
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevent the default form submission behavior
 
-                                            <form>
+    fetch(`https://localhost:7049/api/user/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    })
+      .then((res) => {
+        console.log("Login successful");
+        
+        if (res.ok) {
+          setRedirect(true);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data)
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
 
-                                                <div className="d-flex align-items-center">
-                                                    <i className="fas fa-car-side fa-2x me-3 thirdColor" ></i>
-                                                    <span className="h1 fw-bold">Login</span>
-                                                </div>
+  if (redirect) {
+    return <Redirect to="/passengerrequestride" />; // put redirect page here on successful registration
+  }
 
-                                                <h5 className="fw-normal mb-3 pb-3" >Sign into your account</h5>
-
-                                                <div data-mdb-input-init className="form-outline mb-4">
-                                                    <label className="form-label" for="form2Example17">Email address</label>
-                                                    <input type="email" id="form2Example17" className="form-control form-control-lg" />
-                                                </div>
-
-                                                <div data-mdb-input-init className="form-outline mb-4">
-                                                    <label className="form-label" for="form2Example27">Password</label>
-                                                    <input type="password" id="form2Example27" className="form-control form-control-lg" />
-                                                </div>
-
-                                                <div className="pt-1 mb-4">
-                                                    <button data-mdb-button-init data-mdb-ripple-init className="btn btn-dark btn-lg btn-block" type="button">Login</button>
-                                                </div>
-
-                                                <p >Don't have an account? <Link to='/registermainpage'>Register here</Link></p>
-                                            </form>
-
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+  return (
+    <div>
+      <section className=" secondColor">
+        <div className="container my-5">
+          <div className="row d-flex justify-content-center align-items-center ">
+            <div className="col col-xl-10">
+              <div className="card border rounded-3">
+                <div className="row g-0">
+                  <div className="col-md-6 col-lg-5 d-none d-md-block">
+                    <img
+                      src={image}
+                      alt="login form"
+                      className="img-fluid rounded-3"
+                    />
+                  </div>
+                  <div className="col-md-6 col-lg-7 d-flex align-items-center">
+                    <div className="card-body  p-lg-5 text-black">
+                      <form onSubmit={(e) => handleSubmit(e)}>
+                        <div className="d-flex align-items-center">
+                          <i className="fas fa-car-side fa-2x me-3 thirdColor"></i>
+                          <span className="h1 fw-bold">Login</span>
                         </div>
-                    </div>
-                </div>
-            </section>
-        </div>
 
-    );
-}
+                        <h5 className="fw-normal mb-3 pb-3">
+                          Sign into your account
+                        </h5>
+
+                        <div data-mdb-input-init className="form-outline mb-4">
+                          <label className="form-label" for="form2Example17">
+                            Email address
+                          </label>
+                          <input
+                            type="email"
+                            id="form2Example17"
+                            className="form-control form-control-lg"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                          />
+                        </div>
+
+                        <div data-mdb-input-init className="form-outline mb-4">
+                          <label className="form-label" for="form2Example27">
+                            Password
+                          </label>
+                          <input
+                            type="password"
+                            id="form2Example27"
+                            className="form-control form-control-lg"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                          />
+                        </div>
+
+                        <div className="pt-1 mb-4">
+                          <button
+                            data-mdb-button-init
+                            data-mdb-ripple-init
+                            className="btn btn-dark btn-lg btn-block"
+                            type="submit"
+                          >
+                            Login
+                          </button>
+                        </div>
+
+                        <p>
+                          Don't have an account?{" "}
+                          <Link to="/registermainpage">Register here</Link>
+                        </p>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
 
 export default LoginPage;
--- src/Pages/RegisterPassenger/RegisterPassenger.jsx
+++ src/Pages/RegisterPassenger/RegisterPassenger.jsx
@@ -1,71 +1,150 @@
-import { Link } from 'react-router-dom';
-import './RegisterPassenger.css'
+import { useState, useEffect, useReducer } from "react";
+import { Link, Redirect } from "react-router-dom";
+import "./RegisterPassenger.css";
 
 const RegisterPassenger = () => {
-    return (
-        <div>
-            <div className='mainDiv my-5'>
-                <div className="container">
-                    <div className="row d-flex justify-content-center align-items-center ">
-                        <div className=" col-md-9  col-xl-6">
-                            <div className="card">
-                                <h2 className="text-center mb-5">Passenger Registeration</h2>
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
 
-                                <form>
-                                    <div data-mdb-input-init className="form-outline mb-4">
-                                        <label className="form-label" for="">User Name</label>
-                                        <input type="text" id="" className="form-control form-control-lg" />
-                                    </div>
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevent the default form submission behavior
 
-                                    <div data-mdb-input-init className="form-outline mb-4">
-                                        <label className="form-label" for="">Email</label>
-                                        <input type="email" id="" className="form-control form-control-lg" />
-                                    </div>
+    fetch(`https://localhost:7049/api/user/add-passenger`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: name,
+        phoneNumber: phoneNumber,
+        email: email,
+        password: password,
+      }),
+    })
+      .then((res) => {
+        console.log("Passenger Added to the database");
+        if (res.ok) {
+          setRedirect(true);
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
 
-                                    <div data-mdb-input-init className="form-outline mb-4">
-                                        <label className="form-label" for="">Phone Number</label>
-                                        <input type="tel" name="phone" id="phone" className="form-control form-control-lg" />
-                                    </div>
+  if (redirect) {
+    return <Redirect to="/checkforapprove" />; // put redirect page here on successful registration
+  }
 
-                                    <div data-mdb-input-init className="form-outline mb-4">
-                                        <label className="form-label" for="">Password</label>
-                                        <input type="password" id="" className="form-control form-control-lg" />
-                                    </div>
+  return (
+    <div>
+      <div className="mainDiv my-5">
+        <div className="container">
+          <div className="row d-flex justify-content-center align-items-center ">
+            <div className=" col-md-9  col-xl-6">
+              <div className="card">
+                <h2 className="text-center mb-5">Passenger Registeration</h2>
 
-                                    <div data-mdb-input-init className="form-outline mb-4">
-                                        <label className="form-label" for="">Confirm password</label>
-                                        <input type="password" id="" className="form-control form-control-lg" />
-                                    </div>
+                <form onSubmit={(e) => handleSubmit(e)}>
+                  <div data-mdb-input-init className="form-outline mb-4">
+                    <label className="form-label" for="">
+                      User Name
+                    </label>
+                    <input
+                      type="text"
+                      id=""
+                      className="form-control form-control-lg"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                  </div>
 
+                  <div data-mdb-input-init className="form-outline mb-4">
+                    <label className="form-label" for="">
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      id=""
+                      className="form-control form-control-lg"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </div>
 
+                  <div data-mdb-input-init className="form-outline mb-4">
+                    <label className="form-label" for="">
+                      Phone Number
+                    </label>
+                    <input
+                      type="tel"
+                      name="phone"
+                      id="phone"
+                      className="form-control form-control-lg"
+                      value={phoneNumber}
+                      onChange={(e) => setPhoneNumber(e.target.value)}
+                    />
+                  </div>
 
-                                    <div className="form-check d-flex justify-content-center mb-5">
-                                        <input className="form-check-input me-2 border border-1 border-black" type="checkbox" value="" id="" />
-                                        <label className="form-check-label" for="">
-                                            I agree all statements in <a href="#!" className="text-body"><u>Terms of service</u></a>
-                                        </label>
-                                    </div>
+                  <div data-mdb-input-init className="form-outline mb-4">
+                    <label className="form-label" for="">
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      id=""
+                      className="form-control form-control-lg"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
 
-                                    <div className="d-flex justify-content-center">
-                                        <Link to='registermainpage' class="btn btnRegister m-2 ">
-                                            Back
-                                        </Link>
+                  <div className="form-check d-flex justify-content-center mb-5">
+                    <input
+                      className="form-check-input me-2 border border-1 border-black"
+                      type="checkbox"
+                      value=""
+                      id=""
+                    />
+                    <label className="form-check-label" for="">
+                      I agree all statements in{" "}
+                      <a href="#!" className="text-body">
+                        <u>Terms of service</u>
+                      </a>
+                    </label>
+                  </div>
 
-                                        <Link to='/passengerrequestride' class="btn btnRegister m-2 ">
-                                            Register
-                                        </Link>
-                                    </div>
+                  <div className="d-flex justify-content-center">
+                    <button className="btn">
+                      <Link to="registermainpage" class="btn btnRegister m-2 ">
+                        Back
+                      </Link>
+                    </button>
 
-                                    <p className="text-center text-muted mt-5 mb-0">Already have an account? <Link to='/loginpage'
-                                        className="fw-bold text-body"><>Login here</></Link></p>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                    {/* instead of using <Link> I used <Redirect> component
+                        that checks the status of redirect variable at the start of the page
+                    */}
+
+                    <button type="submit" className="btn">
+                      Register
+                    </button>
+                  </div>
+
+                  <p className="text-center text-muted mt-5 mb-0">
+                    Already have an account?{" "}
+                    <Link to="/loginpage" className="fw-bold text-body">
+                      <>Login here</>
+                    </Link>
+                  </p>
+                </form>
+              </div>
             </div>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default RegisterPassenger;
